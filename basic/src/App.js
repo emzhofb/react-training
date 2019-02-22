e98f5ed8262ab5c1b1f3948eeb60bf709ef9f3ec
emzhofb <ikhdamuhammad@gmail.com>
2019-02-22 13:26:39 +0700

    nested navigation, not done yet

--- basic/src/App.js
+++ basic/src/App.js
@@ -9,18 +9,17 @@ const BasicExample = () => (
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/ikhda">Ikhda</Link>
+          <Link to="/about">About Me</Link>
         </li>
         <li>
-          <Link to="/muhammad">Muhammad</Link>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
 
       <hr />
 
       <Route exact path="/" component={Home} />
-      <Route path="/ikhda" component={Ikhda} />
-      <Route path="/muhammad" component={Muhammad} />
+      <Route path="/about" component={Abouts} />
     </div>
   </Router>
 );
@@ -31,19 +30,54 @@ const Home = () => (
   </div>
 );
 
-const Ikhda = () => (
+const Abouts = ({ match }) => (
   <div>
-    <h2>Ikhda</h2>
-    <br />
-    <p>Ikhda adalah nama depan saya, ikhda sepengetahuan saya mempunyai arti salah satu</p>
+    <h2>My Name</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/ikhda`}>Ikhda</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/muhammad`}>Muhammad</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/wildani`}>Wildani</Link>
+      </li>
+    </ul>
+
+    {/* <Route path={`${match.url}/ikhda`} component={Ikhda} />
+    <Route path={`${match.url}/muhammad`} component={Muhammad} />
+    <Route path={`${match.url}/wildani`} component={Wildani} /> */}
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a name.</h3>}
+    />
   </div>
 );
 
-const Muhammad = () => (
-  <div>
-    <h2>Muhammad</h2>
-    <br />
-    <p>Muhammad adalah nama tengah, nama ini sangat bagus, saya bersyukur mempunyai nama ini dan arti dari nama ini adalah orang yang terpuji.</p>
-  </div>
-);
+// const Ikhda = () => {
+//   <div>
+//     <h2>Ikhda</h2>
+//     <br />
+//     <p>Ikhda adalah nama depan saya, ikhda sepengetahuan saya mempunyai arti salah satu</p>
+//   </div>
+// }
+
+// const Muhammad = () => {
+//   <div>
+//     <h2>Muhammad</h2>
+//     <br />
+//     <p>Muhammad adalah nama tengah, nama ini sangat bagus, saya bersyukur mempunyai nama ini dan arti dari nama ini adalah orang yang terpuji.</p>
+//   </div>
+// }
+
+// const Wildani = () => {
+//   <div>
+//     <h2>Wildani</h2>
+//     <br />
+//     <p>Dan Wildani adalah nama akhir saya, wildan sendiri berarti anak laki laki dari surga, dan tambahan huruf ya' dalam bahasa arab sendiri berati aku, jadi bisa di artikan anak laki laki ku dari surga</p>
+//   </div>
+// }
+
 export default BasicExample
